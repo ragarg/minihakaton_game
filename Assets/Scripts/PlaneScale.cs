ce60656be60cf7856de8ab529ee15537689d780d
Draugr deathlord Horker <dhorker@oa-j3.21-school.ru>
2019-11-09 15:54:48 +0300

    PlaneScale, PlayerMove

---
+++ Assets/Scripts/PlaneScale.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneScale : MonoBehaviour
+{
+    [ContextMenu("PlanePosition")]
+        private void PlanePosition()
+        {
+            transform.position = new Vector3(2.0f, 0.0f, (transform.localScale.z / 2) * 10);
+        }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-0.1f, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(0.1f, 0.0f, 0.0f);
+        }
+        transform.position -= new Vector3(0, 0, 0.1f * speed);
+    }
+}
