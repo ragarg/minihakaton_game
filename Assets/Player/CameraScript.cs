542a6678d1268fb553a7bac02bd1599649e81d4a
Kodlak Meera reed <kmeera-r@oa-j2.21-school.ru>
2019-11-09 15:39:03 +0300

    player add

---
+++ Assets/Player/CameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform target;
+
+    private Vector3 delta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        delta = transform.position - target.GetChild(2).transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.GetChild(2).transform.position + delta;
+    }
+}
