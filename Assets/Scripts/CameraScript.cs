d969f5fe679d18a5e0d543aeb8d10918e084aa03
Kodlak Meera reed <kmeera-r@oa-j2.21-school.ru>
2019-11-09 18:39:38 +0300

    player strafe and camera

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -7,16 +7,18 @@ public class CameraScript : MonoBehaviour
     public Transform target;
 
     private Vector3 delta;
+    private Vector3 x;
 
     // Start is called before the first frame update
     void Start()
     {
         delta = transform.position - target.position;
+        x = transform.position;
     }
-
     // Update is called once per frame
     void Update()
     {
         transform.position = target.position + delta;
+        transform.position = new Vector3(x.x, transform.position.y, transform.position.z);
     }
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float speed;
+    private string right = "right";
+    private string left = "left";
 
     void Update()
     {
+        transform.GetComponent<Animator>().SetBool(right, false);
+        transform.GetComponent<Animator>().SetBool(left, false);
         if (Input.GetKey(KeyCode.A))
-        {
+        { 
             transform.position += Vector3.left * 0.1f;
+            transform.GetComponent<Animator>().SetBool(left, true);
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.position += Vector3.right * 0.1f;
+            transform.GetComponent<Animator>().SetBool(right, true);
         }
         transform.position += Vector3.forward * speed;
     }
